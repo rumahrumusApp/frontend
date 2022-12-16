eca3435fd9e734f7fa9ef95ec267975e93df7501
rumikadms <rumikadamayantims@gmail.com>
2022-12-16 14:45:37 +0700

    update

---
+++ .next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/dasboard":["static\u002Fchunks\u002Fpages\u002Fdasboard.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fdasboard"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
